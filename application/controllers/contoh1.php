d4f7bd2ed37084fcb0ac162da570a0391159578a
Leonando12 <leonandosyahputra@gmail.com>
2021-09-21 20:29:39 +0700

    menambahkan/install codeigniter

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+....public function index()
+....{
+..... echo "<h1> mybio </h1>";
+..... echo "nama....... :Leo Nando Syah Putra <br>
+..... tempat lahir : Bekasi
+..... tanggal lahir : 12 mei 1999";
+..... }
+}
